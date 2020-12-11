bc656f3dd07cdd127d5ecff4e560a82f96949bd3
david <dcastilloa@uni.pe>
2020-12-11 16:03:04 -0500

    Uv4l updated to stream

--- uv4l_install/install_uv4l.sh
+++ uv4l_install/install_uv4l.sh
@@ -1,1 +1,8 @@
+
+# Agregar la siguiente linea al archivo /etc/apt/sources.list
+# deb https://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main
+# despues correr este script
+
+curl https://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
+sudo apt-get update
 sudo apt-get install uv4l uv4l-webrtc uv4l-uvc uv4l-server
