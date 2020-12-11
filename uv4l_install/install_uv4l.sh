3e91eaa439c80e239c0fd0c489d596abd3ac01b6
david <dcastilloa@uni.pe>
2020-12-10 19:10:05 -0500

    uv4l install requirements

---
+++ uv4l_install/install_uv4l.sh
@@ -0,0 +1,1 @@
+sudo apt-get install uv4l uv4l-webrtc uv4l-uvc uv4l-server
---
+++ uv4l_install/server.sh
@@ -0,0 +1,8 @@
+uv4l --auto-video_nr \
+--driver uvc \
+--device-id=046d:082b \
+--server-option '--port=9090' \
+--server-option '--enable-webrtc=yes' \
+--server-option '--enable-webrtc-video=yes' \
+--server-option '--enable-webrtc-audio=no' \
+--server-option '--enable-webrtc-datachannels=no' \
